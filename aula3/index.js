655c8fd58ebbeea1f413b87b99dd145b3a66c426
Dennys Fernandes <dennys.projetos@outlook.com>
2025-03-07 19:32:31 -0300

    Aula5 variáveis

---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Dennys "Fernandes"');
+console.log("Dennys 'Fernandes'"); // '', "", ``, São considerados String = Texto
+console.log(`'Dennys' "Fernandes"`);
+
+console.log(35, 15.85); // Number = Número
---
+++ aula6/index.js
@@ -0,0 +1,39 @@
+let nome = "Henrique";
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho de", nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
+
+let nome; // Declarou a variável
+nome = 'João'; //Inicializando a Variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisa ter nomes significativos
+let nomeCliente = "João";
+console.log(nomeCliente);
+
+// Não pode começar o nome de uma variável com um número
+let no1me
+// Não pode conter espaços ou traços
+let nome-Cliente
+// Utilizamos camemlCase
+let nomeCompleto = 'Dennys Fernandes';
+let nomeCompletodoCliente = 'Dennys Fernandes';
+// Case-sensitive
+let nomeCliente = 'Luiz';
+let nomecliente = 'Otávio';
+
+console.log(nomeCliente, nomecliente);
+// Não podemos redeclarar variáveis com let
+let nomeCliente = 'Luiz';
+nomeCliente = 'Otávio';
+
+console.log(nomeCliente);
+
+// NÃO UTILIZE VAR, UTILIZE LET.
