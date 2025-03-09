44f49dea4507e9c05eef02e1eda0e25d22172a18
Dennys Fernandes <dennys.projetos@outlook.com>
2025-03-09 16:39:10 -0300

    aula11 alert. config e prompt

---
+++ aula11/JS/script.js
@@ -0,0 +1,12 @@
+// alert('A nossa mensagem.')
+// window.confirm('Deseja acessar a página ?')
+// window.prompt('Digite seu nome para acessar a pagina')
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1)
+num2 = Number(num2)
+const resultado = (num1 + num2)
+
+alert (`A soma dos valores resultará em: ${resultado}`)
