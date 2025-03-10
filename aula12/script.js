a8f072fbf39ac139c2af04a32244ce7d0b32874f
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-10 11:52:35 -0300

    att aula 12

--- aula12/script.js
+++ aula12/script.js
@@ -3,8 +3,10 @@ let varB = "B"; //C
 let varC = "C"; //A
 
 const varAtemp = varA;
-varA = varC // B
-varB = varB // C
-varC = varAtemp // A
- 
+varA = varC; // B
+varB = varB; // C
+varC = varAtemp; // A
+console.log(varA, varB, varC);
+
+[varA, varB, varC] = [varB, varC, varA];
 console.log(varA, varB, varC);
