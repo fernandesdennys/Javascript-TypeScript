258435b8537fe3f6234b872f67000e8c387a22af
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-11 20:05:20 -0300

    Objeto Math

---
+++ aula15 Objeto Math/script.js
@@ -0,0 +1,22 @@
+let num1 = 9;
+console.log(num1 ** (1 / 2));
+console.log(num1 ** 0.5); // Para achara raiz quadrada.
+
+console.log(100 / 0);
+console.log(!!(100 / 0)); //true
+
+// let num2 = Math.floor(num1); // Arredonda para o numero inteiro mais proximo, para baixo.
+// let num2 = Math.ceil(num1); // Arredonda para o numero inteiro mais proximo, para cima.
+//let num2 = Math.round(num1); // Aredonda de acordo com as casas decimais acima de 0.5 para cima e 0.49 para baixo
+console.log(
+  Math.max(1, 2, 3, 4, 5, 6, 7, 8, 2, 4235, 425, 2342, 26362, 42352352, 14215)
+); //Me mostra o maior numero da lista
+console.log(
+  Math.min(1, 2, 3, 4, 5, 6, 7, 8, 2, 4235, 425, 2342, 26362, 42352352, 14215)
+); //Me mostra o menor numero da lista
+console.log(Math.random()); // Mostra um numero aleatório
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); //Serve para aleatoriedade de números inteiros para numeros flutuantes "const aleatorio = Math.random() * (10 - 5) + 5;
+
+console.log(Math.PI);
+console.log(Math.pow(2, 10)); // Potenciação
+console.log(2 ** 10);
