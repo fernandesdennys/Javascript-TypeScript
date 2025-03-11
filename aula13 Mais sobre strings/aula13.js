0202dea5f0737bc0670a63658f24de144930985d
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-11 16:14:42 -0300

    Update aula13

--- aula13 Mais sobre strings/aula13.js
+++ aula13 Mais sobre strings/aula13.js
@@ -7,12 +7,11 @@ console.log(umaString[3]); // Busca o indice dentro da string
 console.log(umaString.concat(" em", " um", " lindo dia."));
 console.log(umaString + " em um lindo dia.");
 console.log(`${umaString} em um lindo dia.`);
-
 console.log(umaString.indexOf("texto")); // Começa no indice 3 em relação a frase "Um texto".
 console.log(umaString.indexOf("o", 4)); // Acha a Letra "o" depois do indice 4.
 console.log(umaString.lastIndexOf("m", 3)); // Acha a letra de traz para frente a partir do indice 3.
-console.log(umaString.match(/[a-z]/g)); // Expressão regular (mandando a flag "g")
-console.log(umaString.match(/[a-z]/));
+console.log(umaString.match(/[a-z]/g)); // Expressão regular (mandando a flag global "g").
+console.log(umaString.match(/[a-zA-Z]/)); // Expressão regular que captura qualquer letra maiúscula dentro da string.
 console.log(umaString.search(/x/)); // Pedindo para encontrar a letra "x" (igual ao indexOf, porem ele aceita expressões  regulares).
 console.log(umaString.replace("Um", "Outro")); // Substitui uma palavra por outra.
 console.log(umaString.replace(/Um/, "Outro")); // Substitui uma palavra por outra usando expressão regular.
