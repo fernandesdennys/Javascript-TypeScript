d41826eff2c5664089468ba64518634a18f24ae4
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-12 03:38:50 -0300

    ex01 aula 015

--- aula15 Objeto Math/ex01/js/script.js
+++ aula15 Objeto Math/ex01/js/script.js
@@ -1,5 +1,10 @@
-const numero =  prompt('Digite um numero:');
+let numero =  Number(prompt('Digite um numero:'));
 const nTitulo = document.getElementById('numero-titulo');
 const dTitulo = document.getElementById('texto')
 
 nTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Raiz quadrada: <strong>${(numero ** 0.5)}<strong></p>`;
+texto.innerHTML = `<p>${numero}é inteiro:</p>`;
+texto.innerHTML = `<p>É NaN: ${numero.ceil(numero)}</p>`;
+texto.innerHTML = `<p>Arredondado para cima: ${Math.round(numero)}</p>`;
+texto.innerHTML = `<p>Com duas casas decimais: ${numero}</p>`;
--- aula15 Objeto Math/script.js
+++ aula15 Objeto Math/script.js
@@ -1,22 +1,19 @@
 let num1 = 9;
 console.log(num1 ** (1 / 2));
-console.log(num1 ** 0.5); // Para achara raiz quadrada.
+console.log(num1 ** 0.5); // Para achar a raiz quadrada.
+console.log(Math.sqrt(16)); // Retorna 4
 
-console.log(100 / 0);
+console.log(100 / 0); // Infinity
 console.log(!!(100 / 0)); //true
 
-// let num2 = Math.floor(num1); // Arredonda para o numero inteiro mais proximo, para baixo.
-// let num2 = Math.ceil(num1); // Arredonda para o numero inteiro mais proximo, para cima.
-//let num2 = Math.round(num1); // Aredonda de acordo com as casas decimais acima de 0.5 para cima e 0.49 para baixo
-console.log(
-  Math.max(1, 2, 3, 4, 5, 6, 7, 8, 2, 1500, 245, 4205, 15924, 1099433, 1425115)
-); //Me mostra o maior numero da lista
-console.log(
-  Math.min(1, 2, 3, 4, 5, 6, 7, 8, 2, 1500, 425, 4225, 26362, 42352352, 15215)
-); //Me mostra o menor numero da lista
-console.log(Math.random()); // Mostra um numero aleatório
-const aleatorio = Math.round(Math.random() * (10 - 5) + 5); //Serve para aleatoriedade de números inteiros para numeros flutuantes "const aleatorio = Math.random() * (10 - 5) + 5;
+let num2 = Math.floor(num1); // Arredonda para o numero inteiro mais proximo, para baixo.
+let num3 = Math.ceil(num1); // Arredonda para o numero inteiro mais proximo, para cima.
+let num4 = Math.round(num1); // Aredonda de acordo com as casas decimais acima de 0.5 para cima e 0.49 para baixo
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 2, 1500, 245, 4205, 15924, 1099433, 1425115)); // Me mostra o maior numero da lista
+console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 2, 1500, 425, 4225, 26362, 42352352, 15215)); // Me mostra o menor numero da lista
+console.log(Math.random()); // Mostra um numero aleatório entre 0 e 1 sendo que 1 nao é incluido.
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Serve para aleatoriedade de números inteiros para numeros flutuantes "const aleatorio = Math.random() * (10 - 5) + 5;
 
-console.log(Math.PI);
+console.log(Math.PI); // Valor de PI
 console.log(Math.pow(2, 10)); // Potenciação
 console.log(2 ** 10); // Potenciação
