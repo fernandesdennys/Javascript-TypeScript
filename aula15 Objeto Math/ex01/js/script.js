8ed92b1cdf316c2af1c436a362e3fd8245867803
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-11 21:24:30 -0300

    aula15 Objeto Math

---
+++ aula15 Objeto Math/ex01/js/script.js
@@ -0,0 +1,5 @@
+const numero =  prompt('Digite um numero:');
+const nTitulo = document.getElementById('numero-titulo');
+const dTitulo = document.getElementById('texto')
+
+nTitulo.innerHTML = numero;
