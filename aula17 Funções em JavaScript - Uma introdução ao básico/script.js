339089eeb797d80cf67c71d9cded476d0694321e
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-13 14:23:56 -0300

    Att. Aula 17 Funções

--- aula17 Funções em JavaScript - Uma introdução ao básico/script.js
+++ aula17 Funções em JavaScript - Uma introdução ao básico/script.js
@@ -1,12 +1,20 @@
-// MODO SIMPLIFICADO
+const raiz =  function(n) {
+  return n ** 0.5; 
+}; 
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
 
+
+/*
+
+// MODO SIMPLIFICADO
 const raiz = n => n ** 0.5; 
 
 console.log(raiz(9));
 console.log(raiz(16));
 console.log(raiz(25));
 
-/* 
 const raiz =  (n) => {
   return n ** 0.5; 
 }; 
