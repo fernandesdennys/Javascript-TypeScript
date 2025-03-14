04961ff52488ca79ef5a000046176402ef092bdd
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-13 23:17:16 -0300

    Aula 19 Valores primitivos

---
+++ aula19 Valores primitivos e valores por referência/ex/JS/script.js
@@ -0,0 +1,47 @@
+function clicar() {
+  const form = document.querySelector('.form');
+  const resultado = document.getElementById('res');
+  const pessoas = [];
+
+  function recebeEventoForm(evento) {
+    evento.preventDefault(); // Impede o recarregamento da página
+
+    // Pegamos os valores dentro do evento (agora pega os dados preenchidos no form)
+    const nome = form.querySelector('#txtNome');
+    const sobrenome = form.querySelector('#txtSobrenome');
+    const peso = form.querySelector('#txtPeso');
+    const altura = form.querySelector('#txtAltura');
+    
+    // Criamos o objeto com os valores no momento do submit
+    const resFrom = {
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: Number(peso.value),
+      altura: Number(altura.value)
+    };
+
+    // Adicionamos ao array
+    pessoas.push(resFrom)
+    resultado.innerHTML += (`<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`)
+    // Resetamos o formulário
+    form.reset();
+    // Exibimos o array no console
+    console.log(pessoas);
+  }
+  // Adicionamos o evento corretamente  
+  form.addEventListener('submit', recebeEventoForm);
+
+};
+clicar()
+
+
+/*
+form.onsubmit = function (evento) {
+  evento.preventDefault();
+  alert(1);
+  cosole.log(eviado);
+};
+
+*/
+
+
