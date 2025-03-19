aee5928ce7fb9112e96b34f3418301c76b95ddfe
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-19 18:08:12 -0300

    Update

---
+++ TREINO/treino.js
@@ -0,0 +1,15 @@
+/* 
+function soma(num1, num2) {
+  return num1 + num2;
+}
+console.log(soma(3, 7)) 
+*/
+
+//----------------------------------------
+
+function contarAte(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i)
+  }
+}
+contarAte(20)
