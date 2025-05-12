6cdd32dd1dd76634b85537ec0f1017f5de3d704f
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-05-12 18:46:38 -0300

    Métdos úteis para objetos Ot´ávio Miranda

--- aula72 Object.defineProperty() Object.defineProperties()/script.js
+++ aula72 Object.defineProperty() Object.defineProperties()/script.js
@@ -33,6 +33,7 @@ function criaProduto() {
 const p2 = criaProduto('Camiseta');
 p2.nome = 'Dennys Ricardo Fernandes'
 console.log(p2.nome)
+
 /* const p1 = new Produto('Camiseta', 20, 3);
 // console.log(p1)
 p1.estoque = 500
