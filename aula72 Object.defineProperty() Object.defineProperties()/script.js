e0f0f9effeca29376e3c211a886fa68ef6a7a8bf
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-05-12 15:04:03 -0300

    aula71 Object.defineProperty() Object.defineProperties()B Otávio Miranda

--- aula72 Object.defineProperty() Object.defineProperties()/script.js
+++ aula72 Object.defineProperty() Object.defineProperties()/script.js
@@ -2,17 +2,40 @@
 function Produto(nome, preco, estoque) {
   this.nome = nome;
   this.preco = preco;
-  
+  let estoquePrivado = estoque
+
   Object.defineProperty(this, 'estoque', {
-    enumerable: true, // Mostra a chave
-    value: estoque, // Valor
-    writable: false, // Pode alterar 
-    configurable: true, // Configurável (não permite apagar e reconfigurar a variável)
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      return estoquePrivado;
+    },
+    set: function (valor) {
+      if (typeof valor !== 'number') {
+        throw new TypeError('Err');
+      }
+      estoquePrivado = valor;
+    }
   });
 }
-const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1)
 
-for (let chave in p1) {
-  console.log(chave);
+function criaProduto() {
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      valor = valor.replace('Ricardo', '')
+      nome = valor;
+    }
+  }
 }
+const p2 = criaProduto('Camiseta');
+p2.nome = 'Dennys Ricardo Fernandes'
+console.log(p2.nome)
+/* const p1 = new Produto('Camiseta', 20, 3);
+// console.log(p1)
+p1.estoque = 500
+console.log(p1.estoque) */
+
+
